dec046eebbe04d4b69c2ab0a975375ed7c5b62f2
Daniel Richards <daniel.richards-8@students.plymouth.ac.uk>
2019-10-09 10:53:35 +0100

    Added solution for Multiplication&DivisionCalculator exercise.

---
+++ Multiplication&DivisionCalculator/script.js
@@ -0,0 +1,13 @@
+function multiplyBy(){
+	num1 = document.getElementById("number1").value;
+	num2 = document.getElementById("number2").value;
+
+	document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy(){
+	num1 = document.getElementById("number1").value;
+	num2 = document.getElementById("number2").value;
+
+	document.getElementById("result").innerHTML = num1 / num2;
+}
